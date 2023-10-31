220abd28d13be240403372467706ee2aca738c54
Ioannis Michail <kadafi16@googlemail.com>
2023-10-31 08:32:50 +0100

    Projektdateien hinzufügen.

---
+++ GO.Workerservice/Program.cs
@@ -0,0 +1,17 @@
+namespace GO.Workerservice
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IHost host = Host.CreateDefaultBuilder(args)
+                .ConfigureServices(services =>
+                {
+                    services.AddHostedService<Worker>();
+                })
+                .Build();
+
+            host.Run();
+        }
+    }
+}
