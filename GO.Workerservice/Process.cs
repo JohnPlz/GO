f92cc264c195202218015b31a0d48fa76d86c14b
Philip Schwientek <philip@psch-notebook.fritz.box>
2023-11-29 13:33:53 +0100

    Added PackageData.

--- GO.Workerservice/Process.cs
+++ GO.Workerservice/Process.cs
@@ -1,17 +1,20 @@
 namespace GO.Workerservice;
 
-public class Process {
+public class Process 
+{
 
     private DatabaseService DatabaseService;
     private ScaleDimensionerResult ScaleDimensionerResult;
 
-    public Process(DatabaseService databaseService, ScaleDimensionerResult scaleDimensionerResult) {
+    public Process(DatabaseService databaseService, ScaleDimensionerResult scaleDimensionerResult) 
+    {
         this.DatabaseService = databaseService;
         this.ScaleDimensionerResult = scaleDimensionerResult;
     }
 
-    public async Task ProcessPackageAsync() {
-        
+    public async Task ProcessPackageAsync() 
+    {
+
     }
 
 }
