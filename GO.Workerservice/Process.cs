5f725d7cd3975efb38a4280bd5bac1c3a3f4ac48
Philip Schwientek <philip@psch-notebook.fritz.box>
2023-11-29 12:48:52 +0100

    Added Process.

---
+++ GO.Workerservice/Process.cs
@@ -0,0 +1,17 @@
+namespace GO.Workerservice;
+
+public class Process {
+
+    private DatabaseService DatabaseService;
+    private ScaleDimensionerResult ScaleDimensionerResult;
+
+    public Process(DatabaseService databaseService, ScaleDimensionerResult scaleDimensionerResult) {
+        this.DatabaseService = databaseService;
+        this.ScaleDimensionerResult = scaleDimensionerResult;
+    }
+
+    public async Task ProcessPackageAsync() {
+        
+    }
+
+}
