e8b620c923b298947d0c70391b2d62a90b4fc8b5
Philip Schwientek <philip@psch-notebook.fritz.box>
2023-12-02 21:08:49 +0100

    DatabaseConfiguration and appsettings changed..

--- GO.Workerservice/DatabaseConfiguration.cs
+++ GO.Workerservice/DatabaseConfiguration.cs
@@ -4,26 +4,30 @@ public sealed class DatabaseConfiguration
 {
     public required string Host { get; set; } = null!;
 
-    public int Port { get; set; } = 3306;
-
     public required string Username { get; set; } = null!;
 
     public string? Password { get; set; } = null;
 
     public required string Database { get; set; } = null!;
+    public required string Driver { get; set; } = null!;
+
+    public required string EngineName { get; set; } = null!;
+    
 
     internal string GenerateConnectionString()
     {
-        // Server=myServerAddress;Port=1234;Database=myDataBase;Uid=myUsername;Pwd=myPassword;
+        // Driver={SQL Anywhere 10};DatabaseName=godus;EngineName=test;uid=budde;pwd=PASSWORD;LINKs=tcpip(host=192.168.103.201)
 
         string connectionString = "";
-        connectionString += $"Server={this.Host};";
-        connectionString += $"Port={this.Port};";
+        connectionString += "Driver={" + this.Driver + "};";
         connectionString += $"Database={this.Database};";
-        connectionString += $"Uid={this.Username};";
+        connectionString += $"EngineName={this.EngineName};";
+        connectionString += $"uid={this.Username};";
         if (this.Password is not null) {
-            connectionString += $"Pwd={this.Password};";
+            connectionString += $"pwd={this.Password};";
         }
+        connectionString += $"LINKs=tcpip(host={this.Host});";
+
         return connectionString;  
     }
 }
